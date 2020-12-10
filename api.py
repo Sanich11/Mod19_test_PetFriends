d58d967303eedbea10a7b980fd9aa58c8b577f5e
Alexander <70514174+Sanich11@users.noreply.github.com>
2020-12-10 15:04:28 +0300

    update

--- api.py
+++ api.py
@@ -85,7 +85,8 @@ class PetFriends:
 
         res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
         status = res.status_code
-        result = ""
+        result = ''
+
         try:
             result = res.json()
         except json.decoder.JSONDecodeError:
@@ -113,4 +114,50 @@ class PetFriends:
             result = res.json()
         except json.decoder.JSONDecodeError:
             result = res.text
+        return status, result
+
+    def add_new_pet_without_photo(self, auth_key: json, name: str,
+                                  animal_type: str, age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце без фото и возвращает
+        статус запроса на сервер и результат в формате JSON с данными добавленного питомца.
+        Здесь отрабатывается POST API запрос."""
+
+        data = {
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+            }
+
+        headers = {'auth_key': auth_key['key']}
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ''
+
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+    def add_foto_of_pet(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
+        """Метод отправляет запрос на сервер на добавление данных питомца - фото - по указанному ID
+        и возвращает статус запроса и result в формате JSON с обновлённыи данными питомца.
+        Здесь отрабатывается POST API запрос."""
+
+        data = MultipartEncoder(
+            fields={'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')})
+
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + 'api/pets' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ''
+
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
         return status, result
